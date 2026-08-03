fb02585e6111cd5197624abcc9ffcccfa2525466
Dante Luca Ortega <dlortega@fi.uba.ar>
2026-08-03 00:14:07 -0300

    Cambios a routes

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/productos', require('./producto'));
-router.use('/compras', require('./compras'));
+router.use('/compras', require('./compra'));
 
 
 
