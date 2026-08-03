42746eaaf5978da898532e260bc37af386fad435
Dante Luca Ortega <dlortega@fi.uba.ar>
2026-08-02 23:46:18 -0300

    Update index.js

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/productos', require('./producto'));
-router.use('/ventas', require('./venta'));
+router.use('/ventas', require('./ventas'));
 
 
 module.exports = router;
