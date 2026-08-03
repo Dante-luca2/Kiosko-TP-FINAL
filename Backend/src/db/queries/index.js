79a0ce86d3ea71673cd449abe82b51dfad93c706
Dante Luca Ortega <dlortega@fi.uba.ar>
2026-08-03 00:31:28 -0300

    agregar CRUD de categorias
    - Queries en db/queries/categoria.js: getAll, getById, create, update
    - Endpoints en routes/categoria.js: GET /, GET /:id, POST /, PUT /:id
      (sin DELETE, es una tabla de referencia simple)
    - Validacion de nombre obligatorio y manejo de nombre duplicado (23505)
    - Montado el router en routes/index.js y agregado a queries/index.js

--- Backend/src/db/queries/index.js
+++ Backend/src/db/queries/index.js
@@ -1,3 +1,4 @@
 module.exports = {
   producto: require('./producto'),
+  categoria: require('./categoria'),
 };
--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/productos', require('./producto'));
+router.use('/categorias', require('./categoria'));
 
 
 
