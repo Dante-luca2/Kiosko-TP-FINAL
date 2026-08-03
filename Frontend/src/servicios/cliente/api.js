33536df1a0e16766d8ea258465d831857af0c5fa
Dante Luca Ortega <dlortega@fi.uba.ar>
2026-08-02 22:01:21 -0300

    Update api.js

--- Frontend/src/servicios/cliente/api.js
+++ Frontend/src/servicios/cliente/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://localhost:3000/api",
+    baseURL: "http://localhost:3000/api",
     timeout: 5000,
     headers: { "X-Custom-Header": "foobar" },
 });
